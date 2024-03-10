c95c1c4a4627cff4b32bb5b1829cfc61236d0f9a
Plopmenz <plopmenz@gmail.com>
2024-03-10 21:20:06 +0100

    check for existing deployment

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -3,6 +3,7 @@ import { DeployAdminSettings, deployAdmin } from "./Admin/OpenmeshAdmin";
 
 export interface OpenmeshAdminDeploymentSettings {
   adminSettings: DeployAdminSettings;
+  forceRedeploy?: boolean;
 }
 
 export interface OpenmeshAdminDeployment {
@@ -13,6 +14,10 @@ export async function deploy(
   deployer: Deployer,
   settings?: OpenmeshAdminDeploymentSettings
 ): Promise<OpenmeshAdminDeployment> {
+  if (settings?.forceRedeploy !== undefined && !settings.forceRedeploy) {
+    return await deployer.loadDeployment({ deploymentName: "latest.json" });
+  }
+
   const admin = await deployAdmin(deployer, settings?.adminSettings ?? {});
 
   const deployment = {
