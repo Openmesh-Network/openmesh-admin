74a33d7547bd5c94ed20f9f95cde062ab1f825b0
Plopmenz <plopmenz@gmail.com>
2024-03-31 00:40:17 +0100

    smart account update

--- src/Openmesh.sol
+++ src/Openmesh.sol
@@ -2,5 +2,5 @@
 pragma solidity ^0.8.0;
 
 abstract contract Openmesh {
-    address constant OPENMESH_ADMIN = 0x57A20c301e02517baB06d5deDF4bb6d568Ee441b;
+    address constant OPENMESH_ADMIN = 0x5fE15253481Ab95A55e3d1287B27aFcF7BEa4861;
 }
--- src/OpenmeshAdmin.sol
+++ src/OpenmeshAdmin.sol
@@ -2,7 +2,15 @@
 pragma solidity ^0.8.0;
 
 import {SmartAccount} from "../lib/smart-account/src/SmartAccount.sol";
+import {SmartAccountBaseInstaller} from "../lib/smart-account/src/SmartAccountBaseInstaller.sol";
+
+address constant installer = 0x4811864B715C0F1B0f9790a8ee6F11dC25b7F258;
 
 contract OpenmeshAdmin is SmartAccount {
-    constructor() SmartAccount(0x2309762aAcA0a8F689463a42c0A6A84BE3A7ea51) {}
+    constructor()
+        SmartAccount(
+            installer,
+            abi.encodeWithSelector(SmartAccountBaseInstaller.install.selector, 0x2309762aAcA0a8F689463a42c0A6A84BE3A7ea51)
+        )
+    {}
 }
