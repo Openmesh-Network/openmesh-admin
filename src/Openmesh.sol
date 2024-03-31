6a3a114e3a8a67a20cd8adf5a905291377c7a201
Plopmenz <plopmenz@gmail.com>
2024-03-31 17:38:14 +0200

    admin address update

--- src/Openmesh.sol
+++ src/Openmesh.sol
@@ -2,5 +2,5 @@
 pragma solidity ^0.8.0;
 
 abstract contract Openmesh {
-    address constant OPENMESH_ADMIN = 0x5fE15253481Ab95A55e3d1287B27aFcF7BEa4861;
+    address constant OPENMESH_ADMIN = 0xaD2dc241DcBbC2F5c413d96C1A779cd6C40791A3;
 }
